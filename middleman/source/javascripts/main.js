0d8a00eb3923824b338691d6ad4992481a0dc194
Marcel Sands <marcel.sands84@gmail.com>
2016-06-15 14:58:53 -0400

    update contact form and footer

--- middleman/source/javascripts/main.js
+++ middleman/source/javascripts/main.js
@@ -3,7 +3,7 @@
 function initializeSite() {
     "use strict";
 
-    // Init effect 
+    // Init effect
     $('#scene').parallax();
 
     // Center and outline
@@ -52,60 +52,6 @@ function initializeSite() {
                 return openerElement.is('img') ? openerElement : openerElement.find('img');
             }
         }
-    });
-
-    // Contact Form
-    $(function() {
-        $('#contact').validate({
-
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                message: {
-                    required: true
-                }
-            },
-
-            messages: {
-                name: {
-                    required: "come on, you have a name don't you?",
-                    minlength: "your name must consist of at least 2 characters"
-                },
-                email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
-                }
-            },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact.php",
-                    success: function() {
-                        $('#contact :input').attr('disabled', 'disabled');
-                        $('#contact').fadeTo( "slow", 0.15, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn();
-                        });
-                    },
-                    error: function() {
-                        $('#contact').fadeTo( "slow", 0.15, function() {
-                            $('#error').fadeIn();
-                        });
-                    }
-                });
-            }
-        });
     });
 
     // MailChimp Integration
@@ -195,13 +141,13 @@ $(window).load(function(){
 -------------------------------------------------------------- */
 $(window).load(function(){
 
-    //SKROLLR 
+    //SKROLLR
     if (Modernizr.touch) {
         skrollr.init().destroy();
-    } else {   
+    } else {
         skrollr.init({
             forceHeight: false
-        });  
+        });
     }
 
 });
